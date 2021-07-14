ccf87c12efea10dd8019834d41ad7daf460ff97d
Haza1410 <85071252+Haza1410@users.noreply.github.com>
2021-07-14 15:05:47 +0100

    Create TradingNEA.py

---
+++ TradingNEA.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import xlswriter
+import requests
+from scipy import stats
+import math
+#Imports the software libraries: NumPy - a numerial computing library, Pandas - data frame for storing data, XLSxWriter - module used to save Pandas data frame as an Excel file, Requests - used to make HTTP requests, SciPy Stats - a module containing probability distributions, summary and frequency statistics, correlation functions and statistical tests, Math - a standard module that allows the use of mathematical functions.
+
+stocks = pd.read_csv("sp_500_stocks.csv")
+stocks["Ticker"]
